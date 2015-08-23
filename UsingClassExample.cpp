7177bd53024041e8a5a4d03d1e91bca432772f04
Frank Botos <frankbotos@hotmail.com>
2015-08-23 09:21:47 -0400

    showing collision detection, player creation

--- UsingClassExample.cpp
+++ UsingClassExample.cpp
@@ -1,18 +1,14 @@
-//This class works only in conjunction with SFML!
-
-//This is just a basic example of how you can set up an SFML window and then use the Character class within that Window's update cycle to import a sprite sheet, and have it be
-//automatically animated and moved based on user input
-
 #include "character.h"
+#include "worldObject.h"
 #include <SFML\Window.hpp>
 #include <SFML\Graphics.hpp>
 
 int main() {
-	sf::RenderWindow w(sf::VideoMode(1280, 720), "Test!");//creating SFML window
+	sf::RenderWindow w(sf::VideoMode(1280, 720), "Test!");
 
-	//Character test("sprites/playerCharacter.png", 32, 32, 5, false);
-	Character test("sprites/playerCharacter8.png", 32, 32, 6, true); //creating Character object - passing in spritesheet directory, width of a single frame, height of a single frame
-																																	//number of frames in each animation, and a bool for 8 directional animation
+	//Character test("sprites/playerCharacter8.png", 32, 32, 6, true, true);
+	Character test("sprites/playerCharacter8.png",32,32,5,true,false);
+	WorldObject block("sprites/block.png",597,434,200,200);
 
 	//begin game loop
 	while (w.isOpen()) {
@@ -22,11 +18,13 @@ int main() {
 			if (event.type == sf::Event::Closed) { w.close(); }//close windows if exit is clicked
 		}
 
-		w.clear();//clearing before drawing new frame
+		w.clear(sf::Color::White);//clearing before drawing new frame
 
 		/*DRAWING IS TO BE DONE BETWEEN CLEAR AND DISPLAY*/
-		test.charMove(); // call the charMove function, which initiates a perpetual loop tha checks for movement and animation
-		w.draw(test.getSpriteObj());//using SFML, draw the sprite associated with the Character object
+		w.draw(block);
+		w.draw(test);
+		test.checkCollision(block);
+		test.charMove();
 
 		w.display();//end current frame, display its contents on the screen
 	}
